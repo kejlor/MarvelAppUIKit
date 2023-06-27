1c4ba80178c4efc103e3cd3304d7e908f05f8e36
kejlor <bartoszwojtkowiak.dev@gmail.com>
2023-06-27 14:10:58 +0200

    Add network service

--- MarvelAppUIKit/Services/NetworkService.swift
+++ MarvelAppUIKit/Services/NetworkService.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class NetworkService {
+    var session = URLSession.shared
+    private var decoder = JSONDecoder()
+    
+    enum NetworkServiceError: Error {
+        case invalidURL
+        case missingData
+    }
+    
+    func fetchData<T: Decodable>(url: String) async throws -> T {
+        guard let url = URL(string: url) else { throw NetworkServiceError.invalidURL }
+        let (data, _) = try await session.data(from: url)
+        return try decoder.decode(T.self, from: data)
+    }
+    
+    func fetchImage(url: String) async throws -> Data? {
+        guard let url = URL(string: url) else { throw NetworkServiceError.invalidURL }
+        let (data, _) = try await session.data(from: url)
+        return data
+    }
+}
