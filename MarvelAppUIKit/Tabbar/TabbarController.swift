d2ebe3b161fbbd122244bf6133c91039ae049902
kejlor <bartoszwojtkowiak.dev@gmail.com>
2023-06-30 09:45:21 +0200

    Add tabbar coordinator

--- MarvelAppUIKit/Tabbar/TabbarController.swift
+++ MarvelAppUIKit/Tabbar/TabbarController.swift
@@ -5,4 +5,24 @@
 //  Created by Bartosz Wojtkowiak on 29/06/2023.
 //
 
-import Foundation
+import UIKit
+
+class TabbarController: UITabBarController {
+    weak var coordinator: TabbarCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func addViewControllers(_ vcs: [UIViewController]) {
+        let items = [UITabBarItem(tabBarSystemItem: .favorites, tag: 0),
+                     UITabBarItem(tabBarSystemItem: .search, tag: 1)]
+                //tu tworzysz itemy dolnego paska
+            setViewControllers(vcs, animated: true)
+            
+            for (index, vc) in vcs.enumerated() {
+                vc.tabBarItem = items[index]
+            }
+        }
+}
+
--- MarvelAppUIKit/Tabbar/TabbarCoordinator.swift
+++ MarvelAppUIKit/Tabbar/TabbarCoordinator.swift
@@ -5,4 +5,42 @@
 //  Created by Bartosz Wojtkowiak on 29/06/2023.
 //
 
-import Foundation
+import UIKit
+
+class TabbarCoordinator: Coordinator {
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+    var tabViewController = TabbarController()
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.navigationController.isNavigationBarHidden = true
+    }
+    
+    func start() {
+        tabViewController.coordinator = self
+        self.setupViewControllers()
+        navigationController.pushViewController(tabViewController, animated: true)
+    }
+    
+    func childDidFinish(_ child: Coordinator) {
+        for (index, coordinator) in childCoordinators.enumerated() {
+            if coordinator === child {
+                childCoordinators.remove(at: index)
+            }
+        }
+    }
+    
+    func setupViewControllers() {
+        let mainCoordinator = MainCoordinator(navigationController: navigationController)
+        mainCoordinator.parent = self
+        let main = mainCoordinator.getComicsListVC()
+        
+            // dodac searchCoordin
+        let searchCoordinator = MainCoordinator(navigationController: navigationController)
+        searchCoordinator.parent = self
+        let search = searchCoordinator.getComicsListVC()
+        
+        tabViewController.addViewControllers([main, search])
+    }
+}
