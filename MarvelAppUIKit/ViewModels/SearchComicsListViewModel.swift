ad50a010c86394d45970c20c6a0f177815c2732d
kejlor <bartoszwojtkowiak.dev@gmail.com>
2023-07-03 10:10:36 +0200

    Clean comics after making fetch request

--- MarvelAppUIKit/ViewModels/SearchComicsListViewModel.swift
+++ MarvelAppUIKit/ViewModels/SearchComicsListViewModel.swift
@@ -22,6 +22,8 @@ final class SearchComicsListViewModel {
     
     func getComicsByTitle(for title: String) async {
         do {
+            filteredComics = []
+            delegate?.didFetchComics(self.filteredComics)
             try await self.filteredComics = comicsRepository.fetchComicsByTitle(title: title).data.results.compactMap(ComicViewModel.init)
             delegate?.didFetchComics(self.filteredComics)
         } catch {
