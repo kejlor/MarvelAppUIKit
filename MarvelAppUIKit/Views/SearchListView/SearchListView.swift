75ff1b29a0e6709e8155ff22e4476a13f05666da
kejlor <bartoszwojtkowiak.dev@gmail.com>
2023-07-04 10:49:11 +0200

    Fix views

--- MarvelAppUIKit/Views/SearchListView/SearchListView.swift
+++ MarvelAppUIKit/Views/SearchListView/SearchListView.swift
@@ -13,34 +13,34 @@ class SearchListView: UIView {
         tableView.rowHeight = ComicsCell.rowHeight
         tableView.tableFooterView = UIView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.isHidden = false
-        tableView.backgroundColor = .cyan
-        tableView.setHeight(350)
-        tableView.rowHeight = 300
         return tableView
     }()
     private(set) lazy var searchTextField: SearchTextField = {
         let textField = SearchTextField(placeHolderText: "SearchListViewControllerSearchTextFieldPlaceholder".localized)
         textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.backgroundColor = .magenta
+        textField.setHeight(SearchListViewParameters.searchTextFieldHeight)
         return textField
     }()
     private(set) lazy var verticalStackView: UIStackView = {
         let stack = UIStackView()
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
-        stack.backgroundColor = .green
         stack.distribution = .equalCentering
-        stack.alignment = .top
-        stack.setHeight(500)
+        stack.alignment = .center
         return stack
     }()
     private(set) lazy var emptySearchView = {
         let empty = EmptySearchListView(listStatus: .emptyList)
         empty.translatesAutoresizingMaskIntoConstraints = false
-        empty.backgroundColor = .yellow
-        empty.isHidden = true
+        empty.isHidden = false
+        empty.setHeight(SearchListViewParameters.emptySearchView)
         return empty
+    }()
+    private(set) lazy var tableContainerView: UIView = {
+        let tableContainer = UIView()
+        tableContainer.translatesAutoresizingMaskIntoConstraints = false
+        tableContainer.isHidden = true
+        return tableContainer
     }()
     
     override init(frame: CGRect) {
@@ -58,29 +58,31 @@ class SearchListView: UIView {
 }
 
 extension SearchListView {
-    
     func layout() {
-        verticalStackView.addArrangedSubview(tableView)
-//        verticalStackView.addArrangedSubview(emptySearchView)
+        verticalStackView.addArrangedSubview(tableContainerView)
+        verticalStackView.addArrangedSubview(emptySearchView)
+        tableContainerView.addSubview(tableView)
         
         addSubviews(searchTextField, verticalStackView)
         
         searchTextField.anchor(top: safeTopAnchor, left: safeLeftAnchor)
         verticalStackView.anchor(top: searchTextField.bottomAnchor, left: safeLeftAnchor, bottom: safeBottomAnchor, right: safeRightAnchor)
+        tableContainerView.fillSuperView()
+        tableView.fillSuperView()
     }
     
     func showTableView() {
-        tableView.isHidden = false
-        let estimatedHeight = 200
+        tableContainerView.isHidden = false
         let width = self.frame.size.width
-        self.tableView.frame = CGRect(x: .zero, y: .zero, width: Int(width), height: estimatedHeight)
+        self.tableView.frame = CGRect(x: .zero, y: .zero, width: Int(width), height: SearchListViewParameters.tableFrameEstimatedHeight)
         emptySearchView.isHidden = true
         self.tableView.reloadData()
     }
     
     func showEmptySarchView() {
+        emptySearchView.checkMessageStatus()
         emptySearchView.isHidden = false
-//        tableView.isHidden = true
+        tableContainerView.isHidden = true
     }
 }
 
@@ -88,4 +90,7 @@ enum SearchListViewParameters {
     static let tableNumberOfRowsInSection: Int = 0
     static let keyboardYDivider: CGFloat = 2
     static let keyboardYMultiplier: CGFloat = -1
+    static let searchTextFieldHeight: CGFloat = 50
+    static let emptySearchView: CGFloat = 150
+    static let tableFrameEstimatedHeight: Int = 200
 }
