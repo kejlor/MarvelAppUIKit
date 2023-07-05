619064db7c162feb3176e7c51f1610e0b1399439
kejlor <bartoszwojtkowiak.dev@gmail.com>
2023-07-05 14:28:35 +0200

    Add controller to container

--- MarvelAppUIKit/Containers/ViewControllerContainer.swift
+++ MarvelAppUIKit/Containers/ViewControllerContainer.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Swinject
+
+class ViewControllerContainer {
+    static let sharedConainer = ViewControllerContainer()
+    let container = Container()
+    let viewModelContainer = ViewModelContainer.sharedContainer
+    
+    private init() {
+        setupContainer()
+    }
+    
+    private func setupContainer() {
+        container.register(SearchListViewController.self) { _ in
+            return SearchListViewController(searchListComicsVM: self.viewModelContainer.container.resolve(SearchComicsListViewModelProtocol.self)!)
+        }
+        
+        container.register(ComicsListViewController.self) { _ in
+            return ComicsListViewController(comicListVM: self.viewModelContainer.container.resolve(ComicListViewModelProtocol.self)!)
+        }
+    }
+}
