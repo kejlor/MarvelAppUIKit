d316378bc7caec6f435176cd6c8d92bb7e6fc576
kejlor <bartoszwojtkowiak.dev@gmail.com>
2023-07-06 12:28:14 +0200

    Remove view controllers

--- MarvelAppUIKit/AppDelegate.swift
+++ MarvelAppUIKit/AppDelegate.swift
@@ -13,25 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     let navController = UINavigationController()
     let tabBarController = UITabBarController()
-    let container: Container = {
-        let container = Container()
-        container.register(NetworkService.self) { _ in
-            NetworkService()
-        }
-        container.register(ComicsRepository.self) { r in
-            let controller = ComicsRepository(networkService: r.resolve(NetworkService.self)!)
-            return controller
-        }
-        container.register(ComicListViewModel.self) { r in
-            ComicListViewModel(comicsRepository: r.resolve(ComicsRepository.self)!)
-        }
-        container.register(ComicsListViewController.self) { r in
-            let controller = ComicsListViewController(comicListVM: r.resolve(ComicListViewModel.self)!)
-            return controller
-        }
-        
-        return container
-    }()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         let coordinator = TabBarCoordinator(navigationController: navController)
