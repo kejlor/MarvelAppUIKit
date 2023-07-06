22edc9603cf228d32218acc0dba2884fc6a5553c
kejlor <bartoszwojtkowiak.dev@gmail.com>
2023-07-06 09:29:50 +0200

    Add container to app delegate

--- MarvelAppUIKit/AppDelegate.swift
+++ MarvelAppUIKit/AppDelegate.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import Swinject
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     let navController = UINavigationController()
     let tabBarController = UITabBarController()
+    let container: Container = {
+        let container = Container()
+        container.register(ComicsListViewController.self) { r in
+            let controller = ComicsListViewController(comicListVM: r.resolve(ComicListViewModel.self)!)
+            return controller
+        }
+        return container
+    }()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         let coordinator = TabBarCoordinator(navigationController: navController)
@@ -21,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = navController
+        window?.rootViewController = container.resolve(ComicsListViewController.self)
         
         return true
     }
